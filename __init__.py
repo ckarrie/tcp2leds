d84cba160046ead2d63344de590859656da32aef
Christian Karrié <christian@karrie.info>
2023-02-13 19:43:34 +0100

    add better API

---
+++ __init__.py
@@ -0,0 +1,139 @@
+from __future__ import annotations
+
+import socket
+import time
+
+
+class LEDProgram:
+    def __init__(self, host, port=10000, used_leds=60, led_offset=0, strip_leds=144):
+        """
+
+        :param host: IP or hostname of running tcp2leds server
+        :param port: Port
+        :param used_leds: amount of leds you want to use in this program
+        :param led_offset: shift leds to right used in this program
+        :param strip_leds: amount of all leds on your LED strip
+        """
+
+        self.host = host
+        self.port = port
+        self.used_leds = used_leds
+        self.led_offset = led_offset
+        self.strip_leds = strip_leds
+
+        self._sections = []
+        self._sock_connection = None
+
+    def add_section(self, section):
+        section.program = self
+        if section not in self._sections:
+            self._sections.append(section)
+
+    @property
+    def sections(self):
+        return self._sections
+
+    def realign_sections_width(self):
+        width_per_section = 100 / len(self._sections)
+        for section in self._sections:
+            print(f"set section {section.name} width from {section.width_percentage}% to {width_per_section}%")
+            section.width_percentage = width_per_section
+
+    def connect(self):
+        # Connect to server
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (self.host, self.port)
+        print(f'starting up on {self.host} port {self.port}')
+        sock.connect(server_address)
+        self._sock_connection = sock
+
+    def generate_led_array_message(self):
+        led_array = ['-'] * self.strip_leds
+        # Todo offset here
+
+        led_index = 0
+        for section in self._sections:
+            # Build leds for each section
+            section.build_leds()
+            # put them in led array
+            for led in section.leds:
+                led_array[led_index] = led.color
+                led_index += 1
+
+        led_array_msg = ''.join(led_array)
+        return led_array_msg
+
+    def clear_leds(self):
+        if self._sock_connection is None:
+            self.connect()
+
+        led_array_msg = '0' * self.strip_leds
+        msg_b = led_array_msg.encode()
+        self._sock_connection.sendall(msg_b)
+
+    def push(self):
+        if self._sock_connection is None:
+            self.connect()
+
+        led_array_msg = self.generate_led_array_message()
+        msg_b = led_array_msg.encode()
+        self._sock_connection.sendall(msg_b)
+
+    def push_loop(self):
+        clear_after_pushes_counter = 0
+        while True:
+            try:
+                self.push()
+                time.sleep(0.1)
+                if clear_after_pushes_counter == 100:
+                    self.clear_leds()
+                    clear_after_pushes_counter = 0
+                clear_after_pushes_counter += 1
+            except KeyboardInterrupt:
+                self._sock_connection.close()
+
+
+class Section:
+    def __init__(self, name, width_percentage=100):
+        self.program: None | LEDProgram = None
+        self.name = name
+        if width_percentage < 0 or width_percentage > 100:
+            raise ValueError(f"width must me lower or equal 100, not `{width_percentage}` (type {type(width_percentage)})")
+        self.width_percentage = width_percentage
+        self.leds = []
+        self.debug = False
+
+    def build_leds(self):
+        raise NotImplementedError("Missing build_leds")
+
+    @property
+    def usable_leds_in_program(self):
+        if self.program:
+            return self.program.used_leds
+        return 0
+
+    @property
+    def usable_leds_in_section(self):
+        if self.program:
+            return int(self.usable_leds_in_program / len(self.program.sections))
+        return 0
+
+
+class LED:
+    COLOR_IGNORE = '-'
+    COLOR_OFF = '0'
+    COLOR_RED = 'r'
+
+    def __init__(self, index_in_section=0, color=COLOR_IGNORE):
+        self.index_in_section = index_in_section
+        self.color = color
+
+    def __repr__(self):
+        return f'<LED{self.index_in_section}: {self.color}>'
+
+
+def test():
+    program = LEDProgram("192.168.178.100")
+    program.add_section(Section(name='green'))
+    program.add_section(Section(name='red'))
+    program.realign_sections_width()
---
+++ homeassistant.py
@@ -0,0 +1,158 @@
+import datetime
+import time
+
+import requests
+from requests import ConnectionError
+
+from . import Section, LED
+
+
+class HomeAssistantSection(Section):
+    def __init__(self, name, url, long_time_token=None, entity_id=None):
+        super().__init__(name=name, width_percentage=100)
+        self.entity_id = entity_id
+        self.hass_url = url
+        self.long_time_token = long_time_token
+        self.state_value = None
+        self.state_update_timedelta = datetime.timedelta(seconds=1)
+        self.state_last_updated = datetime.datetime.now()
+
+    def _convert_state(self, v):
+        return v
+
+    def _get_current_state(self):
+        url = f"{self.hass_url}/api/states/{self.entity_id}"
+        headers = {
+            "Authorization": f"Bearer {self.long_time_token}",
+            "content-type": "application/json",
+        }
+        response = requests.get(url, headers=headers)
+        try:
+            state = response.json().get("state")
+        except ConnectionError:
+            print(f"[ConnectionError] waiting for {url}")
+            time.sleep(10)
+            return self.state_value
+
+        try:
+            state = self._convert_state(state)
+        except (ValueError, TypeError) as e:
+            print(f"Can't read entity `{self.entity_id}` from your HomeAssistant:", str(e))
+
+        return state
+
+    def update_state_value(self):
+        has_old_data = (datetime.datetime.now() - self.state_last_updated) > self.state_update_timedelta
+        if has_old_data or self.state_value is None:
+            self.state_value = self._get_current_state()
+            self.state_last_updated = datetime.datetime.now()
+            if self.debug:
+                print("update!")
+        return self.state_value
+
+
+class HomeAssistantPowerSection(HomeAssistantSection):
+    def __init__(self, name, url, long_time_token=None, entity_id=None, value_per_led=None, stage_colors=None):
+        super().__init__(name, url, long_time_token=long_time_token, entity_id=entity_id)
+        self.value_per_led = value_per_led
+        self.stage_colors = []
+        if isinstance(stage_colors, list):
+            self.stage_colors = stage_colors
+        elif isinstance(stage_colors, str) and len(stage_colors) == 1:
+            self.stage_colors = [stage_colors]
+
+        self.running_light_index = 0
+
+    def _convert_state(self, v):
+        return float(v)
+
+    def get_stage_color(self, stage_index: int, value=None):
+        try:
+            return self.stage_colors[stage_index]
+        except IndexError:
+            return LED.COLOR_RED
+
+    def _pre_build_leds(self):
+        if not self.leds or len(self.leds) != self.usable_leds_in_section:
+            self.leds = []
+            for i in range(self.usable_leds_in_section):
+                led = LED(index_in_section=i)
+                self.leds.append(led)
+
+    def build_leds(self):
+        self._pre_build_leds()
+        usable_leds_in_section = self.usable_leds_in_section
+        if self.debug:
+            print("usable_leds_in_section", usable_leds_in_section)
+
+        split_stages_value = self.value_per_led * usable_leds_in_section
+        if self.debug:
+            print("split_stages_value", split_stages_value, "watt")
+
+        current_value = self.update_state_value()
+        current_value_abs = abs(current_value)
+        if self.debug:
+            print("current_value", current_value)
+
+        last_stage_index = int(current_value_abs / split_stages_value)
+        last_stage_cnt = last_stage_index + 1
+        if self.debug:
+            print("last_stage_index", last_stage_index)
+
+        virtual_led_value = 0
+        running_light_right = current_value > 0
+
+        # prepare running light index value
+        if running_light_right and self.running_light_index == usable_leds_in_section - 1:
+            self.running_light_index = 0
+        if not running_light_right and self.running_light_index == 0:
+            self.running_light_index = usable_leds_in_section - 1
+
+        # run through stages
+        for stage_index in range(last_stage_cnt):
+            stage_led_color = self.get_stage_color(stage_index=stage_index)
+            if self.debug:
+                print(f"stage{stage_index} color {stage_led_color}")
+
+            for section_index in range(usable_leds_in_section):
+                section_led = self.leds[section_index]
+
+                virtual_led_value = virtual_led_value + self.value_per_led
+                led_is_on = virtual_led_value <= current_value_abs
+
+                if self.debug:
+                    print("  - virtual_led_value", virtual_led_value)
+                    print("  - led_is_on", led_is_on)
+
+                if led_is_on:
+                    section_led.color = stage_led_color
+                else:
+                    # Turn LED off only in first stage
+                    if stage_index == 0:
+                        section_led.color = LED.COLOR_OFF
+
+        if self.debug and self.name == 'helper_verbrauch_haus':
+            print(f'{self.name}: {self.state_value} W, running_light_right={running_light_right}, rli={self.running_light_index}')
+
+        self.leds[self.running_light_index].color = LED.COLOR_OFF
+
+        if running_light_right:
+            self.running_light_index += 1
+        else:
+            self.running_light_index -= 1
+
+        # for led_index, led in enumerate(self.leds):
+        #    if self.running_light_index == led_index:
+        #        prev_color = led.color
+        #        led.color = LED.COLOR_OFF
+        #        if led_index > 0 and running_light_right:
+        #            self.leds[led_index - 1].color = prev_color
+        #        if led_index
+        #    if running_light_right:
+        #        self.running_light_index += 1
+        #    else:
+        #        self.running_light_index -= 1
+        #    break
+
+        if self.debug:
+            print("leds", self.leds)
